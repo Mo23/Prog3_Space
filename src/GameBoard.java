7b880e0ff50d8dd2e2e7e51331278aef369ed6a6
maurice <maurice.toschke@stud.hs-hannover.de>
2016-10-11 09:16:59 +0200

    Moved Sound to own File and Make it Running in own Thread -- Still need cleanup and improvment.

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -4,35 +4,27 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineEvent;
-import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class GameBoard extends JFrame implements LineListener{
+public class GameBoard extends JFrame{
 	final private int WIDTH = 1200;
 	final private int HEIGHT = 700;
 	final private int computerX =7;
 	final private int computerY = 0;
-	protected boolean playCompleted;
 	protected int FIXED_HP=100;
 	private Drawing board;
 	public Spieler virus = new Spieler(550,650,FIXED_HP);
 	public ArrayList computer;
-	public static GameBoard gameboard;
+	public Sound sound = new Sound();
+
 	//public Shot shot = new Shot(gegner.getX(),gegner.getY());
 	public GameBoard() throws UnsupportedAudioFileException, IOException, LineUnavailableException
     {
@@ -119,64 +111,7 @@ public class GameBoard extends JFrame implements LineListener{
   
   
 
-	void play() {
-    	String audioFilePath = "/home/maurice/Schreibtisch/Failing_Defense.wav";
-        
-        File audioFile = new File(audioFilePath);
-        	try {
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
- 
-            AudioFormat format = audioStream.getFormat();
- 
-            DataLine.Info info = new DataLine.Info(Clip.class, format);
- 
-            Clip audioClip = (Clip) AudioSystem.getLine(info);
- 
-            audioClip.addLineListener(this);
- 
-            audioClip.open(audioStream);
-             
-            audioClip.start();
-             
-            while (!playCompleted) {
-                // wait for the playback completes
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-            }
-             
-            audioClip.close();
-             
-        } catch (UnsupportedAudioFileException ex) {
-            System.out.println("The specified audio file is not supported.");
-            ex.printStackTrace();
-        } catch (LineUnavailableException ex) {
-            System.out.println("Audio line for playing back is unavailable.");
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            System.out.println("Error playing the audio file.");
-            ex.printStackTrace();
-        }
-         
-    }
-    
-	@Override
-	public void update(LineEvent event) {
-	     LineEvent.Type type = event.getType();
-         
-	        if (type == LineEvent.Type.START) {
-	            System.out.println("Playback started.");
-	             
-	        } else if (type == LineEvent.Type.STOP) {
-	            playCompleted = true;
-	            System.out.println("Playback completed.");
-	        }
-		
-	}
-    
-    
+	
     
    
 
@@ -185,7 +120,7 @@ public class GameBoard extends JFrame implements LineListener{
              @Override
              public void run() {
                 try {
-					gameboard =new GameBoard();
+					new GameBoard();
 				
 		            
 					
