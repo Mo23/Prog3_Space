21913100a5a61297947d867e7b1db739fbd0ad28
maurice <maurice.toschke@stud.hs-hannover.de>
2016-11-01 15:20:43 +0100

    Update more Logik and Codestyle

--- src/Logikadapter.java
+++ src/Logikadapter.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Logikadapter {
 
@@ -11,10 +12,26 @@ public class Logikadapter {
 	private Shot shot = new Shot(spieler.getX(), spieler.getY());
 	private ArrayList<Enemy> enemylist;
 	private Sound sound = new Sound();
+	private Thread updates;
+
+	// Variablen zur Verlangsamung.
+	private Random random = new Random();
+	private int stoppinganimations = 0;
 
 	public Logikadapter() {
 		this.initComputer();
 
+	}
+
+	public Runnable act() {
+		updates = new Thread() {
+			public void run() {
+				movePlayerShot();
+				moveEnemyShot();
+			}
+		};
+		updates.start();
+		return updates;
 	}
 
 	public void initComputer() {
@@ -26,6 +43,37 @@ public class Logikadapter {
 						(computerY + 80 * i));
 				gegner.fired = false;
 				enemylist.add(gegner);
+			}
+		}
+	}
+
+	public void movePlayerShot() {
+		if (shot.getY() > 0)
+			shot.setXY(shot.getX(), shot.getY() - 1);
+		else {
+			shot.fired = false;
+		}
+	}
+
+	public void moveEnemyShot() {
+		int size_a = enemylist.size();
+
+		for (int i = 0; i < size_a; i++) {
+			Enemy e = enemylist.get(i);
+			EnemyShot a = e.geteshot();
+
+			stoppinganimations = random.nextInt(10);
+			int startshot = random.nextInt(8000);
+
+			if (startshot == 0)
+				e.fired = true;
+			if (a.getY() < 1200 && stoppinganimations == 0 && e.fired) {
+				a.setXY(a.getX(), a.y + 2);
+
+			} else if (a.getY() >= 1200) {
+
+				a.setXY(e.getX(), e.getY());
+
 			}
 		}
 	}
--- src/PanelSpiel.java
+++ src/PanelSpiel.java
@@ -1,11 +1,7 @@
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Random;
 
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -14,21 +10,18 @@ import javax.swing.SwingUtilities;
 public class PanelSpiel extends JPanel {
 
 	private boolean running = false; // Still need to be Implemented
-	
-	private TAdapter adapter; //Tastatureingabe Adapter
+
+	private TAdapter adapter; // Tastatureingabe Adapter
 	public Thread gamerunning;
-	// Variablen zur Verlangsamung.
-	private Random random = new Random();
-	private int stoppinganimations = 0;
-	
+
 	public Logikadapter logik;
 
 	public PanelSpiel() {
-		addKeyListener(adapter = new TAdapter());
+		addKeyListener(this.adapter = new TAdapter());
 		setFocusable(true);
 		setBackground(Color.BLACK); // set background color for this JPanel
 		logik = new Logikadapter();
-		running=true;
+		running = true;
 
 	}
 
@@ -37,16 +30,16 @@ public class PanelSpiel extends JPanel {
 		g.setColor(Color.GREEN);
 
 		if (running) {
-			animations(g);}
-		
+			animations(g);
+		}
 
 		SwingUtilities.invokeLater(act());
+		SwingUtilities.invokeLater(logik.act());
 		if (logik.getShot().fired == true) {
 			DrawShot(g);
 		}
 
 	}
-
 
 	public void animations(Graphics g) {
 		g.setColor(Color.cyan);
@@ -57,28 +50,28 @@ public class PanelSpiel extends JPanel {
 
 	}
 
-
-
-
-
 	public void DrawStatus(Graphics g) {
 
-		g.drawString("Lebenspunkte: " + Integer.toString(logik.getSpieler().HP), 0, 680);
+		g.drawString(
+				"Lebenspunkte: " + Integer.toString(logik.getSpieler().HP), 0,
+				680);
 		g.drawString("Score: " + Integer.toString(logik.getScore()), 1110, 680);
 
-		g.drawString("Lebenspunktegegner: " + logik.getEnemylist().get(0).HP, 200, 200);
+		g.drawString("Lebenspunktegegner: " + logik.getEnemylist().get(0).HP,
+				200, 200);
 
 	}
 
 	public void DrawPlayer(Graphics g) {
-		g.drawImage(logik.getSpieler().getImage(), logik.getSpieler().getX(), logik.getSpieler().getY(), this);
+		g.drawImage(logik.getSpieler().getImage(), logik.getSpieler().getX(),
+				logik.getSpieler().getY(), this);
 	}
 
 	public void DrawComputer(Graphics g) {
-		Iterator<Enemy> it = logik.getEnemylist().iterator();
+		int size_a = logik.getEnemylist().size();
 
-		while (it.hasNext()) {
-			Enemy computer = (Enemy) it.next();
+		for (int i = 0; i < size_a; i++) {
+			Enemy computer = logik.getEnemylist().get(i);
 			g.drawImage(computer.getImage(), computer.getX(), computer.getY(),
 					this);
 		}
@@ -86,7 +79,8 @@ public class PanelSpiel extends JPanel {
 	}
 
 	public void DrawShot(Graphics g) {
-		g.drawImage(logik.getShot().getImage(), logik.getShot().getX(), logik.getShot().getY(), this);
+		g.drawImage(logik.getShot().getImage(), logik.getShot().getX(), logik
+				.getShot().getY(), this);
 	}
 
 	public void DrawEnemyShot(Graphics g) {
@@ -95,8 +89,8 @@ public class PanelSpiel extends JPanel {
 		for (int i = 0; i < size_a; i++) {
 			Enemy e = logik.getEnemylist().get(i);
 			EnemyShot b = e.geteshot();
-			if(e.fired)
-			g.drawImage(b.getImage(), b.getX(), b.getY() + 20, this);
+			if (e.fired)
+				g.drawImage(b.getImage(), b.getX(), b.getY(), this);
 		}
 	}
 
@@ -110,34 +104,9 @@ public class PanelSpiel extends JPanel {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				if (logik.getShot().getY() > 0)
-					logik.getShot().setXY(logik.getShot().getX(), logik.getShot().getY() - 1);
-				else {
-					logik.getShot().fired = false;
-				}
 
-				int size_a = logik.getEnemylist().size();
-
-				for (int i = 0; i < size_a; i++) {
-					Enemy e = logik.getEnemylist().get(i);
-					EnemyShot a = e.geteshot();
-
-					stoppinganimations = random.nextInt(10);
-					int startshot = random.nextInt(8000);
-							
-						if(startshot==0)
-							e.fired=true;
-					if (a.getY() < 1200 && stoppinganimations == 0 && e.fired) {
-						a.setXY(a.getX(), a.y + 2);
-
-					} else if (a.getY() >= 1200) {
-
-						a.setXY(e.getX(), e.getY());
-
-					}
-				}
 				repaint();
-			
+
 			}
 
 		};
@@ -145,23 +114,22 @@ public class PanelSpiel extends JPanel {
 		return gamerunning;
 	}
 
-
-
 	private class TAdapter extends KeyAdapter {
 
 		public void keyReleased(KeyEvent e) {
-			
-			if (KeyEvent.VK_SPACE == e.getKeyCode() && logik.getShot().fired == false) {
-				logik.getShot().setXY(logik.getSpieler().getX(), logik.getSpieler().getY() - 40);
+
+			if (KeyEvent.VK_SPACE == e.getKeyCode()
+					&& logik.getShot().fired == false) {
+				logik.getShot().setXY(logik.getSpieler().getX(),
+						logik.getSpieler().getY() - 40);
 				logik.getShot().fired = true;
 			}
 
 		}
 
 		public void keyPressed(KeyEvent e) {
-				logik.getSpieler().keyPressed(e);
+			logik.getSpieler().keyPressed(e);
 
-			
 			if (KeyEvent.VK_ESCAPE == e.getKeyCode()) {
 
 				GameFrame.gameboard.setVisible(false);
